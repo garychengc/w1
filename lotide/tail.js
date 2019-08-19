5c6ed4fb46f2cd1fcac5b352984e59c1bdfb3d43
Gary Chen <garychen.whc@gmail.com>
2019-08-19 18:02:38 +0000

    tail exercise added

---
+++ lotide/tail.js
@@ -0,0 +1,33 @@
+/* assertFunction */
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/* tail function - return everything but the first element */
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+/* assertFunction for array  */
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse".
+assertEqual(result[1], "Labs");  // ensure the second element is "Labs".
+
+
+/* ensure the original array is not modified.  */
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+
+/* Testing Cases */
+const testing1 = [1];
+console.log(tail(testing1));
+
+const testing2 = [];
+console.log(tail(testing2));
